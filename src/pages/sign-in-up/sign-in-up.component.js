e0b2cc423115c465c706822ad78133c2ed82f29e
Arman Rizal <inazrabuu@gmail.com>
2022-01-31 07:57:20 +0700

    created: sign in and sign up component

---
+++ src/pages/sign-in-up/sign-in-up.component.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignIn from '../../components/sign-in/sign-in.component'
+
+import './sign-in-up.styles.scss'
+
+const SignInUp = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  )
+}
+
+export default SignInUp
