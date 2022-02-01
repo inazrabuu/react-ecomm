a85aa2b64b81b8b6aa712a976f9e8ec74b549628
Arman Rizal <inazrabuu@gmail.com>
2022-02-01 13:01:00 +0700

    edited: get props from redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { auth } from '../../firebase/firebase.util'
 
+import { connect } from 'react-redux'
+
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
 import './header.styles.scss'
@@ -30,4 +32,10 @@ const Header = ({ currentUser }) => {
   )
 }
 
-export default Header
+const mapStateToProps = (state) => {
+  return ({
+    currentUser: state.user.currentUser
+  })
+}
+
+export default connect(mapStateToProps)(Header)
