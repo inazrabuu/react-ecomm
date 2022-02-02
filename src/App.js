32a42dfb3bb84b107647d0f8c297b6dc88325048
Arman Rizal <inazrabuu@gmail.com>
2022-02-02 12:37:34 +0700

    added: checkout page route

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import { createStructuredSelector } from 'reselect'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
-import Header from './components/header/header.component'
 import SignInUp from './pages/sign-in-up/sign-in-up.component'
+import CheckoutPage from './pages/checkout/checkout.component'
+
+import Header from './components/header/header.component'
+
 import { auth, createUserProfileDocument } from './firebase/firebase.util'
+
 import { setCurrentUser } from './redux/user/user.action'
 import { selectCurrentUser } from './redux/user/user.selectors'
 
@@ -47,7 +51,12 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to="/" />) : (<SignInUp />)} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route 
+            exact 
+            path='/signin' 
+            render={() => this.props.currentUser ? (<Redirect to="/" />) : (<SignInUp />)} 
+          />
         </Switch>
       </div>
     )
