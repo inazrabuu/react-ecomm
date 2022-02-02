51165e0a3e63b3e9b6ab775b676a462968578883
Arman Rizal <inazrabuu@gmail.com>
2022-02-02 08:25:28 +0700

    created: cart selector

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,16 @@
+import { createSelector } from 'reselect'
+
+const selectCart = (state) => { return state.cart }
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => { return cart.cartItems }
+)
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) => { 
+    return cartItems.reduce(
+      (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+  }
+)
