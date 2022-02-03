cdd4c339cc54e027e261f645724cb8334c2c37e0
Arman Rizal <inazrabuu@gmail.com>
2022-02-03 08:03:36 +0700

    created: shop selector

---
+++ src/redux/shop/shop.selector.js
@@ -0,0 +1,8 @@
+import { createSelector } from 'reselect'
+
+const selectShop = (state) => { return state.shop }
+
+export const selectShopCollections = createSelector(
+  [selectShop],
+  (shop) => { return shop.collections }
+)
