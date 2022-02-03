c886e8aa055f9ef5295f1c027443ccc0bc015a7a
Arman Rizal <inazrabuu@gmail.com>
2022-02-03 09:08:15 +0700

    edited: move render to collection overview component

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,24 +1,13 @@
 import React from "react"
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
-import { selectShopCollections } from "../../redux/shop/shop.selector"
+import CollectionOverview from '../../components/collection-overview/collection-overview.component'
 
-const ShopPage = ({ collections }) => {
+const ShopPage = () => {
   return(
     <div className="shop-page">
-      {
-        collections.map(({id, ...otherCollectionProps}) => {
-          return (<CollectionPreview key={id} {...otherCollectionProps} />)
-        })
-      }
+      <CollectionOverview />
     </div>
   )
 }
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
-})
-
-export default connect(mapStateToProps)(ShopPage)
+export default ShopPage
