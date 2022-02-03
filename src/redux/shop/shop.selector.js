b19c412091aaae65e4e353b36384407953087cae
Arman Rizal <inazrabuu@gmail.com>
2022-02-03 19:23:26 +0700

    edited: hashmap handle

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,14 +1,6 @@
 import { createSelector } from 'reselect'
 import memoize from 'lodash.memoize'
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-}
-
 const selectShop = (state) => { return state.shop }
 
 export const selectShopCollections = createSelector(
@@ -20,7 +12,7 @@ export const selectShopCollection = memoize((collectionUrlParam) => {
   return createSelector(
     [selectShopCollections],
     (collections) => {
-      return collections.find((collection) => { return collection.id === COLLECTION_ID_MAP[collectionUrlParam]})
+      return collections[collectionUrlParam]
     }
   )
 })
