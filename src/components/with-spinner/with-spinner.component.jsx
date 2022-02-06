1617581446c62803e30ab80967d81f5550caf599
Arman Rizal <inazrabuu@gmail.com>
2022-02-06 16:20:34 +0700

    created: with-spinner HOC

---
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import {
+  SpinnerOverlay,
+  SpinnerContainer
+} from './with-spinner.styles'
+
+const WithSpinner = (WrappedComponent) => {
+  return ({ isLoading, ...otherProps }) => {
+    return isLoading ? (
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
+    ) : (
+      <WrappedComponent {...otherProps} />
+    )
+  }
+}
+
+export default WithSpinner
