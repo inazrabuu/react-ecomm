724f4a79c0d68eb4b5dfbd3397c1089042b5b454
Arman Rizal <inazrabuu@gmail.com>
2022-02-07 07:38:38 +0700

    added: new actions

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,8 +1,44 @@
 import ShopActionTypes from "./shop.types";
+import { 
+  firestore, 
+  convertCollectionSnapshotToMap 
+} from "../../firebase/firebase.util";
 
 export const updateCollections = (collectionsMap) => {
   return ({
     type: ShopActionTypes.UPDATE_COLLECTIONS,
     payload: collectionsMap
   })
+}
+
+export const fetchCollectionsStart = () => {
+  return ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START
+  })
+}
+
+export const fetchCollectionsSuccess = (collectionsMap) => {
+  return ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+    payload: collectionsMap
+  })
+}
+
+export const fetchCollectionsFailure = (errorMessage) => {
+  return ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage
+  })
+}
+
+export const fetchCollectionStartAsync = () => {
+  return (dispatch) => {
+    const collectionRef = firestore.collection('collections')
+    dispatch(fetchCollectionsStart())
+
+    collectionRef.get().then(async (snapshot) => {
+      const collectionsMap = convertCollectionSnapshotToMap(snapshot)
+      dispatch(fetchCollectionsSuccess(collectionsMap))
+    }).catch((error) => { dispatch(fetchCollectionsFailure(error.message)) })
+  }
 }
