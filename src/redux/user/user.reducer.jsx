b922c02cf4854d3a965f5db4ddbdfbb3a2aef89d
Arman Rizal <inazrabuu@gmail.com>
2022-02-08 20:29:11 +0700

    edited: refactor sign in success & failure

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,4 +1,4 @@
-import { userActionTypes } from "./user.types"
+import userActionTypes from "./user.types"
 
 const INITIAL_STATE = {
   currentUser: null
@@ -6,10 +6,16 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case userActionTypes.SET_CURRENT_USER:
+    case userActionTypes.SIGN_IN_SUCCESS:
       return {
         ...state,
-        currentUser: action.payload
+        currentUser: action.payload,
+        error: null
+      }
+    case userActionTypes.SIGN_IN_FAILURE:
+      return {
+        ...state,
+        error: action.payload
       }
     default:
       return state
