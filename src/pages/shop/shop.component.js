55eb619c4b4b3f49fd7f3bc261f395288bcd7379
Arman Rizal <inazrabuu@gmail.com>
2022-02-08 07:27:30 +0700

    edited: move thunk to saga

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { connect } from "react-redux"
 
-import { fetchCollectionStartAsync } from "../../redux/shop/shop.actions"
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions"
 
 import CollectionOverviewContainer from '../../components/collection-overview/collection-overview.container'
 import CollectionPageContainer from "../collection/collection.container"
@@ -10,8 +10,8 @@ import CollectionPageContainer from "../collection/collection.container"
 
 class ShopPage extends React.Component {
   componentDidMount() {
-    const { fetchCollectionStartAsync } = this.props
-    fetchCollectionStartAsync()
+    const { fetchCollectionsStart } = this.props
+    fetchCollectionsStart()
   }
 
   render() {
@@ -33,7 +33,7 @@ class ShopPage extends React.Component {
 
 const mapDispatchToProps = (dispatch) => {
   return ({
-    fetchCollectionStartAsync: () => { dispatch(fetchCollectionStartAsync()) }
+    fetchCollectionsStart: () => { dispatch(fetchCollectionsStart()) }
   })
 }
 
