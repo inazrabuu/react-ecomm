095708932a35c15c7884cde02e772381fcdd4245
Arman Rizal <inazrabuu@gmail.com>
2022-02-09 08:37:10 +0700

    added: export shopSagas

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects'
+import { takeLatest, call, put, all } from 'redux-saga/effects'
 
 import ShopActionTypes from './shop.types'
 
@@ -22,4 +22,10 @@ export function* fetchCollectionsAsync() {
 
 export function* fetchCollectionsStart() {
   yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync)
+}
+
+export function* shopSagas() {
+  all([
+    call(fetchCollectionsStart)
+  ])
 }
