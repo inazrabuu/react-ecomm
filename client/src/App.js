deebfe81dc4c1edc766884379eefd59ae6bcd363
Arman Rizal <inazrabuu@gmail.com>
2022-02-26 20:51:28 +0700

    edited: lazy load more components

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,6 @@ import React, { useEffect, lazy, Suspense } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 
-import ShopPage from './pages/shop/shop.component'
-import SignInUp from './pages/sign-in-up/sign-in-up.component'
-import CheckoutPage from './pages/checkout/checkout.component'
 import { GlobalStyle } from './global.styles'
 
 import Header from './components/header/header.component'
@@ -13,6 +10,9 @@ import { selectCurrentUser } from './redux/user/user.selectors'
 import { checkUserSession } from './redux/user/user.action'
 
 const HomePage = lazy(() => { return import('./pages/homepage/homepage.component') })
+const ShopPage = lazy(() => { return import('./pages/shop/shop.component') }) 
+const SignInUp = lazy(() => { return import('./pages/sign-in-up/sign-in-up.component') })
+const CheckoutPage = lazy(() => { return import('./pages/checkout/checkout.component') })
 
 const App = () => {
   const currentUser = useSelector(selectCurrentUser)
@@ -29,14 +29,14 @@ const App = () => {
       <Switch>
         <Suspense fallback={<div>...Loading</div>}>
           <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route 
+            exact 
+            path='/signin' 
+            render={() => currentUser ? (<Redirect to="/" />) : (<SignInUp />)} 
+            />
         </Suspense>
-        <Route path='/shop' component={ShopPage} />
-        <Route exact path='/checkout' component={CheckoutPage} />
-        <Route 
-          exact 
-          path='/signin' 
-          render={() => currentUser ? (<Redirect to="/" />) : (<SignInUp />)} 
-        />
       </Switch>
     </div>
   )
