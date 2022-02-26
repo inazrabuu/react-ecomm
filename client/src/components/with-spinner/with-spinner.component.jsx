1abfcf4aef8605917881f386434cb681f6794ee4
Arman Rizal <inazrabuu@gmail.com>
2022-02-26 20:58:43 +0700

    edited: import spinner component

--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 
-import {
-  SpinnerOverlay,
-  SpinnerContainer
-} from './with-spinner.styles'
+import Spinner from '../spinner/spinner.component'
 
 const WithSpinner = (WrappedComponent) => {
   return ({ isLoading, ...otherProps }) => {
     return isLoading ? (
-      <SpinnerOverlay>
-        <SpinnerContainer />
-      </SpinnerOverlay>
+      <Spinner />
     ) : (
       <WrappedComponent {...otherProps} />
     )
