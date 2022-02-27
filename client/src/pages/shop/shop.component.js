861e675ebb23fab4ff5ae7f42af080737cdf8402
Arman Rizal <inazrabuu@gmail.com>
2022-02-27 07:56:32 +0700

    added: lazy, suspense, spinner

--- client/src/pages/shop/shop.component.js
+++ client/src/pages/shop/shop.component.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from "react"
+import React, { useEffect, lazy, Suspense } from "react"
 import { Route } from "react-router-dom"
 import { connect } from "react-redux"
 
 import { fetchCollectionsStart } from "../../redux/shop/shop.actions"
+import Spinner from '../../components/spinner/spinner.component'
 
-import CollectionOverviewContainer from '../../components/collection-overview/collection-overview.container'
-import CollectionPageContainer from "../collection/collection.container"
+const CollectionOverviewContainer = lazy(() => { return import('../../components/collection-overview/collection-overview.container') })
+const CollectionPageContainer = lazy(() => { return import("../collection/collection.container") })
 
 const ShopPage = ({ fetchCollectionsStart, match }) => {
   useEffect(() => {
@@ -14,13 +15,15 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
 
   return(
     <div className="shop-page">
-      <Route 
-        exact 
-        path={`${match.path}`} 
-        component={CollectionOverviewContainer} />
-      <Route 
-        path={`${match.path}/:collectionId`} 
-        component={CollectionPageContainer} />
+      <Suspense fallback={<Spinner />}>
+        <Route 
+          exact 
+          path={`${match.path}`} 
+          component={CollectionOverviewContainer} />
+        <Route 
+          path={`${match.path}/:collectionId`} 
+          component={CollectionPageContainer} />
+      </Suspense>
     </div>
   )
 }
